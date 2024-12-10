aaae06ec09af29a00ccd5297acd7294aa5104a91
GabrielF <gabrielFMendonca@gmail.com>
2024-12-09 22:44:41 -0300

    Adicionado icones na tela de boas vindas

--- src/components/step/index.tsx
+++ src/components/step/index.tsx
@@ -1,4 +1,5 @@
 import { View, Text } from "react-native"
+import { IconProps} from "@tabler/icons-react-native"
 
 import { colors } from "@/styles/theme"
 import { s } from "./style"
@@ -6,10 +7,12 @@ import { s } from "./style"
 type Props = {
     title: string
     description: string
+    icon: React.ComponentType<IconProps>
 }
-export function Step({ title, description }: Props) {
+export function Step({ title, description, icon: Icon }: Props) {
     return(
         <View style={s.container}>
+            {Icon && <Icon size={32} color={colors.red.base}/>}
             <View style={s.details}>
                 <Text style={s.title}>{title}</Text>
                 <Text style={s.description}>{description}</Text>
